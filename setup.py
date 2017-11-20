eac62a4b51308296e9acd39673b0ff9826562810
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2017-11-20 22:12:31 +0200

    adding new file setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='git_cli',
+   version='1.0',
+   description='A useful module for python cli commands',
+   author='Angelos Sakellaropoulos',
+   author_email='angsakel@gmail.com',
+   python_requires='==2.7',
+   install_requires=['mock'],
+)
